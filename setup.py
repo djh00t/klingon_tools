771faaeaf1719939cc91cf19df14ba4721fe191c
David Hooton <david@hooton.org>
2024-06-04 15:42:56 +1000

    Update version number to 0.0.2
    Signed-off-by: David Hooton <david@hooton.org>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="klingon_tools",
-    version="0.0.0",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
         # No external dependencies required
