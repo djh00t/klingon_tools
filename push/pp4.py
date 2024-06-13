adfac2ae0322b92d717eb4bc3ffbe2fe9727ea42
David Hooton <david@hooton.org>
2024-06-13 06:31:42 +0000

    chore(push/pp4.py): update logger output - Updated logger message to include a space after the "😀" emoji.

--- push/pp4.py
+++ push/pp4.py
@@ -466,5 +466,5 @@ else:
             break
 
 # Say bye bye
-logger.info(message="All files processed. Exiting script.", status="😀")
+logger.info(message="All files processed. Exiting script.", status="😀 ")
 logger.info(message=79 * "=", status="")
