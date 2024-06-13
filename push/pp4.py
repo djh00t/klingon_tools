a7299f30a2c4133e4a93b0ef6b13af1317e896f8
David Hooton <david@hooton.org>
2024-06-13 07:34:33 +0000

    feat(push/pp4.py): update log message style to 'default'
     - Update the default log message style from 'basic' to 'default' in the
     'push/pp4.py' file.

--- push/pp4.py
+++ push/pp4.py
@@ -20,7 +20,7 @@ log_tools = LogTools()
 logger = log_tools.log_message
 
 # Set the global log message style
-log_tools.set_default_style("basic")
+log_tools.set_default_style("default")
 
 # Initialize the OpenAI API client
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
