7032a84d92dc46ce7f3e0c3fb27eb792056e0291
David Hooton <david@hooton.org>
2024-06-28 17:59:30 +1000

    ✨ feat(specific-scope): add new landing page layout
    Adding a new landing page layout to enhance user experience.
    
    Signed-off-by: David Hooton <david@hooton.org>

---
+++ .releaserc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  branches: [
+    `release-${process.env.EPOCH_TIME}`
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": "python setup.py sdist bdist_wheel"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "CHANGELOG.md",
+          "setup.py",
+          "version.py"
+        ],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        "branch": "release"
+      }
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": "git add CHANGELOG.md setup.py version.py && git commit -m 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}' && git push origin release"
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        "successComment": false,
+        "failComment": false,
+        "addReleases": "bottom",
+        "createRelease": true,
+        "assets": "dist/*"
+      }
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        "publishCmd": "gh pr create --title 'chore(release): ${nextRelease.version}' --body 'This PR includes the release ${nextRelease.version}.\n\n${nextRelease.notes}' --base main --head release"
+      }
+    ]
+  ]
+};
