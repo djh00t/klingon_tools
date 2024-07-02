4ff88dd737b6c7d28379d5696786fe7554fef7f1
David Hooton <david@hooton.org>
2024-07-02 16:44:15 +1000

    ♻️ refactor(entrypoints.py): Refactor PR generation functions to use subprocess
                Refactor the PR generation functions in entrypoints.py to use the
    subprocess module for executing git commands instead of os.popen. This change
    improves the efficiency and security of the PR generation process.
    
    Signed-off-by: David Hooton <david@hooton.org>

--- klingon_tools/entrypoints.py
+++ klingon_tools/entrypoints.py
@@ -1,5 +1,5 @@
 import logging
-import os
+import subprocess
 
 from klingon_tools.logger import log_tools, logger
 from klingon_tools.openai_tools import OpenAITools
@@ -7,38 +7,57 @@ from klingon_tools.openai_tools import OpenAITools
 # Configure logging to include process name
 log_tools.configure_logging()
 
-
 # Suppress logging from the httpx library
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 def gh_pr_gen_title():
-    # logger.info("Generating PR title using OpenAITools...")
-    diff = os.popen("git diff origin/main..HEAD").read()
+    logger.info("Generating PR title using OpenAITools...")
+    diff = subprocess.run(
+        ["git", "diff", "origin/main..HEAD"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout
     openai_tools = OpenAITools()
     pr_title = openai_tools.generate_pull_request_title(diff)
     print(pr_title)
 
 
 def gh_pr_gen_summary():
-    # logger.info("Generating PR summary using OpenAITools...")
-    diff = os.popen("git diff origin/main..HEAD").read()
+    logger.info("Generating PR summary using OpenAITools...")
+    diff = subprocess.run(
+        ["git", "diff", "origin/main..HEAD"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout
     openai_tools = OpenAITools()
     pr_summary = openai_tools.generate_pull_request_summary(diff)
     print(pr_summary)
 
 
 def gh_pr_gen_context():
-    # logger.info("Generating PR context using OpenAITools...")
-    diff = os.popen("git diff origin/main..HEAD").read()
+    logger.info("Generating PR context using OpenAITools...")
+    diff = subprocess.run(
+        ["git", "diff", "origin/main..HEAD"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout
     openai_tools = OpenAITools()
     pr_context = openai_tools.generate_pull_request_context(diff)
     print(pr_context)
 
 
 def gh_pr_gen_body():
-    # logger.info("Generating PR body using OpenAITools...")
-    diff = os.popen("git diff origin/main..HEAD").read()
+    logger.info("Generating PR body using OpenAITools...")
+    diff = subprocess.run(
+        ["git", "diff", "origin/main..HEAD"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout
     openai_tools = OpenAITools()
     pr_body = openai_tools.generate_pull_request_body(diff)
     print(pr_body)
