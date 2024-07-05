2ca455df007842eccab3d536ea44033ae2786e27
David Hooton <david@hooton.org>
2024-07-05 16:21:22 +1000

    🔧 chore(.releaserc.js):update branches configuration for semantic release plugins
    Signed-off-by: David Hooton <david@hooton.org>

--- .releaserc.js
+++ .releaserc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  branches: [
-    'main',
-    { name: 'release', prerelease: true }
-  ],
+  branches: ["main", { name: "release", prerelease: true }],
   repositoryUrl: "https://github.com/djh00t/klingon_tools.git",
   plugins: [
     "@semantic-release/commit-analyzer",
@@ -11,33 +8,36 @@ module.exports = {
     [
       "@semantic-release/exec",
       {
-        "prepareCmd": "python setup.py sdist bdist_wheel"
-      }
+        prepareCmd: "python setup.py sdist bdist_wheel",
+      },
     ],
     [
       "@semantic-release/git",
       {
-        "assets": ["CHANGELOG.md", "setup.py", "version.py"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
+        assets: ["CHANGELOG.md", "setup.py", "version.py"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
     ],
     [
       "@semantic-release/github",
       {
-        "assets": "dist/*"
-      }
+        assets: "dist/*",
+      },
     ],
     [
       "@semantic-release/exec",
       {
-        "publishCmd": "twine upload dist/* -u __token__ -p $PYPI_TOKEN"
-      }
+        publishCmd: "twine upload dist/* -u __token__ -p $PYPI_TOKEN",
+      },
     ],
     [
       "@semantic-release/exec",
       {
-        "prepareCmd": "gh pr create --title 'chore(release): ${nextRelease.version}' --body 'This PR includes the release ${nextRelease.version}.\n\n${nextRelease.notes}' --base main --head release"
-      }
-    ]
-  ]
+        prepareCmd: `gh pr create --title 'chore(release): \${nextRelease.version}' \
+          --body 'This PR includes the release \${nextRelease.version}.\n\n\${nextRelease.notes}' \
+          --base main --head release`,
+      },
+    ],
+  ],
 };
