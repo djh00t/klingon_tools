4dcdd13872fe43aad9daf3e17a83bfc831565fd0
David Hooton <david@hooton.org>
2024-07-05 13:28:35 +1000

    🔧 chore(logger):update default style to "pre-commit"
    Signed-off-by: David Hooton <david@hooton.org>

--- klingon_tools/logger.py
+++ klingon_tools/logger.py
@@ -16,4 +16,4 @@ from klingon_tools import LogTools
 # Initialize logging
 log_tools = LogTools()
 logger = log_tools.log_message
-log_tools.set_default_style("default")
+log_tools.set_default_style("pre-commit")
