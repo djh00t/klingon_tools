853878581a2c852dcff6e72ad24c7edd0b756367
David Hooton <david@hooton.org>
2024-07-17 17:14:38 +1000

    🔧 chore(release):remove unnecessary semantic-release/exec config and add @semantic-release/github
    Signed-off-by: David Hooton <david@hooton.org>

--- .releaserc.js
+++ .releaserc.js
@@ -8,29 +8,12 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd: "python setup.py set_version ${nextRelease.version} && python setup.py sdist bdist_wheel",
-      },
-    ],
+    "@semantic-release/github",
     [
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "setup.py", "version.py"],
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
-    [
-      "@semantic-release/github",
-      {
-        assets: "dist/*",
-      },
-    ],
-    [
-      "@semantic-release/exec",
-      {
-        publishCmd: "twine upload dist/* -u __token__ -p ${PYPI_TOKEN}",
       },
     ],
   ],
