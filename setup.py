45a7db6409ac18be163c30b304d9478e27f7f45f
David Hooton <david@hooton.org>
2024-07-17 14:39:06 +1000

    🐛 fix(setup.py):update import path for InvalidVersion in setup.py
    Signed-off-by: David Hooton <david@hooton.org>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 import re
 import sys
 from setuptools import find_packages, setup
-from setuptools._vendor.packaging.version import InvalidVersion
+from setuptools.extern.packaging.version import InvalidVersion
 
 
 def get_version():
