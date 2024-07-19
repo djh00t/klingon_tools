53d47f837c6b62515ca24533d22c2075e45ed859
David Hooton <david@hooton.org>
2024-07-19 15:38:25 +1000

    🐛 fix(openai_tools.py):Correct message formatting for logger output
    Signed-off-by: David Hooton <david@hooton.org>

--- klingon_tools/openai_tools.py
+++ klingon_tools/openai_tools.py
@@ -427,7 +427,7 @@ each change of that type under it --> - [ ] `feat`: ✨ A new feature
             formatted_message = self.signoff_message(formatted_message)
 
             logger.info(
-                message="'=' * 80",
+                message="= * 80",
                 status="",
             )
             logger.info(
@@ -435,7 +435,7 @@ each change of that type under it --> - [ ] `feat`: ✨ A new feature
                 status="",
             )
             logger.info(
-                message="'=' * 80",
+                message="= * 80",
                 status="",
             )
 
@@ -457,7 +457,7 @@ each change of that type under it --> - [ ] `feat`: ✨ A new feature
                 )
 
                 logger.info(
-                    message="'=' * 80",
+                    message="= * 80",
                     status="",
                 )
                 logger.info(
@@ -466,7 +466,7 @@ each change of that type under it --> - [ ] `feat`: ✨ A new feature
                     status="",
                 )
                 logger.info(
-                    message="'=' * 80",
+                    message="= * 80",
                     status="",
                 )
 
@@ -610,7 +610,7 @@ each change of that type under it --> - [ ] `feat`: ✨ A new feature
             )
 
         logger.info(
-            message="'=' * 80",
+            message="= * 80",
             status="",
         )
         logger.info(
@@ -618,7 +618,7 @@ each change of that type under it --> - [ ] `feat`: ✨ A new feature
             status="",
         )
         logger.info(
-            message="'=' * 80",
+            message="= * 80",
             status="",
         )
 
