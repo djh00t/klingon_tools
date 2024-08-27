f3fd07034ef8d21c0652e5208011e33f87505f88
David Hooton <david@hooton.org>
2024-08-27 17:44:24 +1000

    ✨ feat(git_unstage): improve functionality to unstage all files
    Refactor `git_unstage_files` to automatically retrieve and unstage all staged
    files
    in the repository instead of relying on an external list. Enhanced logging is
    added
    to indicate the status of operations, including handling the scenario where no
    files
    are staged.
    
    BREAKING CHANGE: The function signature has changed to remove the
    `staged_files`
    argument.
    
    Signed-off-by: David Hooton <david@hooton.org>

--- klingon_tools/git_unstage.py
+++ klingon_tools/git_unstage.py
@@ -3,10 +3,10 @@ from klingon_tools.log_msg import log_message
 from git import exc as git_exc
 
 
-def git_unstage_files(repo: Repo, staged_files: list) -> None:
+def git_unstage_files(repo: Repo) -> None:
     """Unstages all staged files in the given repository.
 
-    This function iterates over all staged files in the repository and
+    This function retrieves all staged files in the repository and
     un-stages them. It logs the status of each file as it is un-staged.
 
     Args:
@@ -16,15 +16,24 @@ def git_unstage_files(repo: Repo, staged_files: list) -> None:
         None
     """
     log_message.info(message="Un-staging staged files", status="🔄")
+
+    # Get the list of staged files
+    staged_files = repo.git.diff("--cached", "--name-only").splitlines()
     log_message.debug(message="Staged files", status=f"{staged_files}")
+
+    if not staged_files:
+        log_message.info(message="No files to unstage", status="ℹ️")
+        return
 
     # Iterate over each staged file and un-stage it
     for file in staged_files:
         try:
-            repo.git.reset("--", file)
+            repo.git.reset("HEAD", file)
             log_message.info(message="Un-staging file", status=f"{file}")
         except git_exc.GitCommandError as e:
             log_message.error(
                 message="Error un-staging file", status=f"{file}"
             )
             log_message.exception(message=f"{e}")
+
+    log_message.info(message="All files unstaged", status="✅")
