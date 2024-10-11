a93b5ff71c36bcbfbbf7c58ac3866976b5ad42db
David Hooton <david@hooton.org>
2024-10-11 17:43:30 +1100

    ✨ feat(log_msg): enhance logging functionality and documentation
    - Updated documentation to clarify the module's purpose and attributes.
    - Added `set_log_level` function to manage log levels for both `log_tools` and
    `log_message`.
    - Introduced log level attributes to `LogTools` for greater clarity and usage
    consistency.
    
    Signed-off-by: David Hooton <david@hooton.org>

--- klingon_tools/log_msg.py
+++ klingon_tools/log_msg.py
@@ -1,23 +1,43 @@
-"""
-logger.py
+"""Initializes logging tools for the application.
 
-This module initializes the logging tools for the application using the
-LogTools class from the klingon_tools package. It sets up the default logging
-style and provides a logger instance for logging messages.
+This module sets up the logging tools using the LogTools class from the
+klingon_tools package. It initializes the logger, sets the default logging
+style, and provides functions for logging and configuration.
 
 Attributes:
     log_tools (LogTools): An instance of the LogTools class for managing
-    logging.
+        logging.
     log_message (LogTools.LogMessage): A logger instance for logging messages.
+    set_log_level (function): Function to set the log level.
+    set_default_style (function): Function to set the default logging style.
 """
 
+import logging
 from klingon_tools import LogTools
 
-# Initialize LogTools with debug set to False by default
+
 log_tools = LogTools(debug=False)
+
+
 log_message = log_tools.log_message
-set_log_level = log_tools.set_log_level
+
+
+def set_log_level(level: str) -> None:
+    """Set the log level for both log_tools and log_message."""
+    log_tools.set_log_level(level)
+    log_message.set_log_level(level)
+
+
 set_default_style = log_tools.set_default_style
+
 
 # Set default logging style
 set_default_style("pre-commit")
+
+
+# Add log level attributes to LogTools
+LogTools.DEBUG = logging.DEBUG
+LogTools.INFO = logging.INFO
+LogTools.WARNING = logging.WARNING
+LogTools.ERROR = logging.ERROR
+LogTools.CRITICAL = logging.CRITICAL
